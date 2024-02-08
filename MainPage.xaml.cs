42b3e08317e0bbe15d348573d7cb3932ac25162a
David Maw <david@maw.name>
2024-02-07 21:12:58 -0800

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace OnePage;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+    private DateOnly date = DateOnly.MinValue;
+
+    public DateOnly Date
+    {
+        get => date; 
+        set
+        {
+            date = value;
+            Debug.WriteLine("Date set to "+date.ToString());
+        }
+    }
+}
